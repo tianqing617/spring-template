d42ca418efa89663f20722c226d66e34043565be
Tianqing Liu <liutq@hollycrm.com>
2023-07-17 11:37:53 +0800

    chore: init mapper

--- src/main/java/top/smartliu/service/impl/AccountServiceImpl.java
+++ src/main/java/top/smartliu/service/impl/AccountServiceImpl.java
@@ -1,16 +1,22 @@
 package top.smartliu.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import top.smartliu.domain.Account;
+import top.smartliu.mapper.AccountMapper;
 import top.smartliu.service.AccountService;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service("accountService")
 public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountMapper accountMapper;
+
     @Override
     public List<Account> findAll() {
         System.out.println("service findAll");
-        List<Account> list = new ArrayList<>();
-        return list;
+        return accountMapper.findAll();
     }
 }
