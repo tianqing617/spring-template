ee49df6808d1430553eabf7f266893f2435bcd56
Tianqing Liu <liutq@hollycrm.com>
2023-07-17 11:14:50 +0800

    chore: init service

--- src/main/java/top/smartliu/controller/AccountController.java
+++ src/main/java/top/smartliu/controller/AccountController.java
@@ -1,15 +1,20 @@
 package top.smartliu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import top.smartliu.service.AccountService;
 
 @Controller
 public class AccountController {
+    @Autowired
+    private AccountService accountService;
     @RequestMapping("/findAll")
     @ResponseBody
     public String findAll() {
         System.out.println("findAll");
+        accountService.findAll();
         return "findAll";
     }
 }
